b04b3ab6bb822f8a0ef77e29fe52cc73c06e1603
huangchenglin1 <huangchenglin1@xiaomi.com>
2023-10-07 20:05:36 +0800

    rust

---
+++ study/string/target/debug/deps/string-2e01597fb0eb0531.d
@@ -0,0 +1,5 @@
+/Users/hcl/work/xiaomi/myGo/mapfeeling/rust/study/string/target/debug/deps/string-2e01597fb0eb0531: src/main.rs
+
+/Users/hcl/work/xiaomi/myGo/mapfeeling/rust/study/string/target/debug/deps/string-2e01597fb0eb0531.d: src/main.rs
+
+src/main.rs:
---
+++ study/string/target/debug/string.d
@@ -0,0 +1,1 @@
+/Users/hcl/work/xiaomi/myGo/mapfeeling/rust/study/string/target/debug/string: /Users/hcl/work/xiaomi/myGo/mapfeeling/rust/study/string/src/main.rs
